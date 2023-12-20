36b179e1c71a371ceba65ea643690cc57b05f33a
HalfSuperNate <96854633+HalfSuperNate@users.noreply.github.com>
2023-12-20 00:43:44 -0600

    Update Lister.sol

--- Lister.sol
+++ Lister.sol
@@ -9,6 +9,7 @@ contract Lister is Admins{
     mapping(address => mapping(uint256 => bool)) public listed;
     mapping(uint256 => bool) public isActiveList;
     mapping(uint256 => uint256) public cost;
+    mapping(uint256 => uint256) public limit;
     uint256 public featuredList;
     address public vault;
 
@@ -23,10 +24,19 @@ contract Lister is Admins{
     /**
      * @dev Admin can set if a list is active to collect.
      * @param _ID The list ID to edit.
-     * @param _cost Flag true for active false for closed.
+     * @param _cost Cost for getting listed.
      */
     function setListCost(uint256 _ID, uint256 _cost) public onlyAdmins {
         cost[_ID] = _cost;
+    }
+
+    /**
+     * @dev Admin can set if a list has a limit.
+     * @param _ID The list ID to edit.
+     * @param _limit Limit at 0 is limitless, otherwise limited at limit.
+     */
+    function setListLimit(uint256 _ID, uint256 _limit) public onlyAdmins {
+        limit[_ID] = _limit;
     }
 
     /**
@@ -47,12 +57,24 @@ contract Lister is Admins{
     }
 
     /**
+     * @dev Admin can list a user on a specified list.
+     * @param _ID The list ID to get listed on.
+     */
+    function listSpecified(uint256 _ID, address _user) public onlyAdmins {
+        if (listed[_user][_ID]) revert AlreadyListed();
+        if (limit[_ID] != 0 && listID[_ID].length >= limit[_ID]) revert ListClosed();
+        listed[_user][_ID] = true;
+        listID[_ID].push(_user);
+    }
+
+    /**
      * @dev User can get listed on a specified list.
      * @param _ID The list ID to get listed on.
      */
     function listMeSpecified(uint256 _ID) public payable {
         if (listed[msg.sender][_ID]) revert AlreadyListed();
         if (!isActiveList[_ID]) revert ListClosed();
+        if (limit[_ID] != 0 && listID[_ID].length >= limit[_ID]) revert ListClosed();
         if (msg.value < cost[_ID]) revert ListCostRequired();
         listed[msg.sender][_ID] = true;
         listID[_ID].push(msg.sender);
